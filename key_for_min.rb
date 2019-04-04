82d16bfdef64f75ca2b175835aa6b463f097ed1e
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-04-04 04:30:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,18 +4,18 @@
 def key_for_min_value(name_hash)
   def key_for_min_value(name_hash)
     name_hash = {:blake => 500, :ashley => 2, :adam => 1}
-   if current_minimum_val == nil 
-        current_minimum_val = value 
-        current_key_assoc_w_min_val = key   
-    else 
-    if current_minimum_val > value 
+   if current_minimum_val == nil
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key
+    else
+    if current_minimum_val > value
           current_minimum_val = value
-          current_key_assoc_w_min_val = key 
+          current_key_assoc_w_min_val = key
         end
       end
-  
-    current_key_assoc_w_min_val 
+
+    current_key_assoc_w_min_val
   end
-  
+
 
 end
